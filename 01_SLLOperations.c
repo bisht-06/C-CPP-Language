7de6c437dec291262e0d86593a68e23602ee5db5
Pankaj <06.bisht@gmail.com>
2022-05-08 16:53:26 +0530

    singly linked list operations program completed.

--- 01_SLLOperations.c
+++ 01_SLLOperations.c
@@ -1,19 +1,17 @@
-/* Singly Linked List Operations */
-
 #include<stdio.h>
 #include<stdlib.h>
 
 void append();
 void addatbegin();
 void addatafter();
-int length();
-void delete();
 void display();
+void delete();
+int length();
 
 struct node
 {
     int data;
-    struct node* link;
+    struct node *link;
 };
 
 struct node* root = NULL;
@@ -22,66 +20,63 @@ int len;
 void main()
 {
     int ch;
+    while(1)
+    {
+        printf("Linked List Operations : \n");
+        printf("1. Append : \n");
+        printf("2. Add Node at Begning : \n");
+        printf("3. Add Node after a Node : \n");
+        printf("4. Display all nodes : \n");
+        printf("5. Delete a node : \n");
+        printf("6. Length of Linked List : \n");
+        printf("7. Exit Program.\n");
 
-        while(1)
+        printf("Enter your choice - \n");
+        scanf("%d",&ch);
+
+        switch (ch)
         {
-            printf("\nLinked List Operations : \n\n");
-            printf("01. Append : \n");
-            printf("02. Add Node at Begining : \n");
-            printf("03. Add a Node after a Node : \n");
-            printf("04. Display all Nodes Information : \n");
-            printf("05. Delete a Node : \n");
-            printf("06. Length of Linked List : \n");
-            printf("07. Exit.\n");
-            printf("\n\n");
-            printf("Enter your Choice : ");
-            scanf("%d",&ch);
+            case 1 :    append();
+                        break;
+                        
+            case 2 :    addatbegin();
+                        break;
 
-            switch (ch)
-            {
-                case 1 :    append();
-                            break;
+            case 3 :    addatafter();
+                        break;
 
-                case 2 :    addatbegin();
-                            break;
+            case 4 :    display();
+                        break;
 
-                case 3 :    addatafter();
-                            break;
+            case 5 :    delete();
+                        break;
 
-                case 4 :    display();
-                            break;
+            case 6 :    len = length();
+                        printf("Linked List has %d nodes.\n",len);
+                        break;
 
-                case 5 :    delete();
-                            break;
+            case 7 :    exit(0);
+                        break;
 
-                case 6 :    len = length();
-                            printf("Length is %d\n",len);
-                            break;
-
-                case 7 :    exit(0);
-                            break;
-
-                default :   printf("Invalid Choice!!\n");
-            }
         }
+    }
 }
-
 
 
 void append()
 {
-    struct node* temp;
+    struct node *temp;
     temp = (struct node*) malloc(sizeof(struct node));
-    printf("Enter Data : ");
+    printf("Enter Node Data : ");
     scanf("%d",&temp->data);
     temp->link = NULL;
-        if (root == NULL)
+        if(root == NULL)
         {
             root = temp;
         }
         else
         {
-            struct node* p;
+            struct node *p;
             p = root;
             while(p->link != NULL)
             {
@@ -89,62 +84,47 @@ void append()
             }
             p->link = temp;
         }
+        system("cls");
 }
 
 void addatbegin()
 {
+    struct node *temp;
+    temp = (struct node *) malloc(sizeof(struct node));
+    printf("Enter node data : ");
+    scanf("%d",&temp->data);
+    temp->link = NULL;
 
+        temp->link = root;
+        root = temp;
 }
 
 void addatafter()
 {
-
-}
-
-int length()
-{
-    int count = 0;
     struct node *temp;
-    temp = root;
-    while(temp != NULL)
-    {
-        count++;
-        temp = temp->link;
-    }
-    return count;
-}
-
-void delete()
-{
-    struct node *temp;
-    int loc;
-    printf("Enter Deletion node : ");
+    int i = 1, loc;
+    printf("Enter the location after which you want insert node : ");
     scanf("%d",&loc);
-    if(loc>length())
-    {
-        printf("\n Invalid Node Address.");
-    }
-    else if(loc==1)
-    {
-        temp = root;
-        root = temp->link;
-        temp->link = NULL;
-        free(temp);
-    }
-    else
-    {
-        struct node *p = root, *q;
-        int i = 1;
-        while(i<loc-1)
+        if(loc>length())
         {
-            p = p->link;
-            i++;
+            printf("Invalid Location!");
         }
-        q = p->link;
-        p->link = q->link;
-        q->link = NULL;
-        free(q);
-    }
+        else
+        {
+            struct node *p = root;
+            while(i<loc)
+            {
+                p = p->link;
+                i++;
+            }
+            temp = (struct node *) malloc(sizeof(struct node));
+            printf("Enter node data : ");
+            scanf("%d",&temp->data);
+            temp->link = NULL;
+            temp->link = p->link;
+            p->link = temp;
+
+        }
 }
 
 void display()
@@ -156,4 +136,51 @@ void display()
         printf("%d-->",temp->data);
         temp = temp->link;
     }
+    system("pause");
+    system("cls");
+}
+
+void delete()
+{
+    struct node *temp = root;
+    int loc;
+    printf("Enter a node location to delete : ");
+    scanf("%d",&loc);
+        if(loc>length())
+        {
+            printf("Invalid Location!!\n");
+        }
+        else if(loc==1)
+        {
+            root = temp->link;
+            temp->link = NULL;
+            free(temp);
+        }
+        else
+        {
+            struct node *p = root, *q;
+            int i = 1;
+            while(i<loc-1)
+            {
+                p = p->link;
+                i++;
+            }
+            q = p->link;
+            p->link = q->link;
+            q->link = NULL;
+            free(q);
+        }
+}
+
+int length()
+{
+    struct node *temp;
+    int count = 0;
+    temp = root;
+    while(temp != NULL)
+    {
+        count++;
+        temp = temp->link;
+    }
+    return count;
 }
